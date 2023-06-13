389fbf690b0bf8b0599c572014a27d1376dc4295
Mohammad <mohammadrasy3@gmail.com>
2023-06-13 16:09:11 -0400

    add todo model and view list todo and edit todo

--- Todo.py
+++ Todo.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 from uuid import uuid4
 from flask import Flask,jsonify,request
 from flask_sqlalchemy import SQLAlchemy
@@ -14,9 +15,11 @@ migrate = Migrate(app, db)
 
 #Models
 class User(db.Model):
+    __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String, unique=True, nullable=False)
     passwordhash = db.Column(db.String(128))
+    todos = db.relationship('Todo', backref='user', lazy='dynamic')
     
     def __repr__(self):
         return self.email
@@ -26,8 +29,47 @@ class User(db.Model):
         
     def check_password(self,password_raw):
         return check_password_hash(self.passwordhash,password_raw)
+
+class  Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer,primary_key=True)
+    author = db.Column(db.Integer, db.ForeignKey('users.id'))
+    title = db.Column(db.String(32),unique=False)
+    note = db.Column(db.Text(),unique=True)
+    done =  db.Column(db.Boolean, default=False)
+    created_at = db.Column(db.DateTime(), default=datetime.utcnow)
     
+    def __repr__(self):
+        return self.title  
     
+    def from_json_update(self,todo_json):
+        title = todo_json.get('title',self.title)
+        done = todo_json.get('done',self.done)
+        note = todo_json.get('note',self.note)
+        self.title = title
+        self.done = done
+        self.note = note
+        db.session.add(self)
+        db.session.commit()
+        return self
+    
+    def get_author(self,author_id):
+        
+        user=User.query.filter_by(id=author_id).first()
+        data={'email':user.email}
+        return data
+    
+    def to_json(self):
+        json_post = {
+        'id':self.id,
+        'note': self.note,
+        'title':self.title,
+        'done':self.done,
+        'created_at': self.created_at,
+        'author':self.get_author(self.author)
+        }
+        return json_post  
+        
 class UserToken(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     token = db.Column(db.String(128),default=str(uuid4()))
@@ -47,6 +89,8 @@ def json_login(json_login_data):
     
     password = json_login_data.get('password')
     email = json_login_data.get('email')
+    if not password and not email:
+        return {'message':'send password and email'}
     user=User.query.filter_by(email=email).first()
     if user is None:
         return {'message':'User Not Found'}
@@ -57,8 +101,24 @@ def json_login(json_login_data):
             return {"message": "Login was success",'token':token}
         else:
             return {'message':'Email,Password is Wrong'}
+        
+def is_author_object(requsted_user,object_author):
+    if requsted_user.id == object_author:
+        return True,{}
+    else:
+        return False,{'messeage':'Must be own todo'}
 
-   
+
+def required_login(req):
+    try:            
+        tk = req.authorization.token
+    except:
+        return {'messeage':'you have to login'},False
+    else:
+        return tk,True
+
+
+      
 def get_user_by_token(token):
     user = UserToken.query.filter_by(token=token).first()
     if user:
@@ -68,5 +128,27 @@ def get_user_by_token(token):
 
 @app.route('/login',methods=['POST'])
 def login():
-    print(get_user_by_token(request.authorization.token))
     return jsonify(json_login(request.json))  
+
+@app.route('/todos')
+def todo_list():
+    data, status = required_login(request)
+    if not status:
+        return jsonify(data)
+    user = get_user_by_token(data)
+    todo_list = Todo.query.filter_by(author=user.id).all()
+    return jsonify({ 'todos': [todo.to_json() for todo in todo_list] })
+    
+@app.route('/todos/<pk>/',methods=['PUT'])
+def todo_edit(pk):
+    data, status = required_login(request)
+    if not status:
+        return jsonify(data)
+    todo = Todo.query.filter_by(id=pk).first()
+    is_author,msg = is_author_object(get_user_by_token(data),todo.author)
+    if is_author:
+        result = todo.from_json_update(request.json)
+        return jsonify({'todo':result.to_json()})
+    else:
+        return jsonify(msg),401
+    
